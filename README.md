2b0ea67733c93127efba3547ed7aae7fc9085a5e
zortazert <zortazert@matthewevan.xyz>
2025-09-27 22:33:46 -0500

    Movement and jump implementation.

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 Setup project working on ball bounce have idea. 2d badminton like mario tennis top down. Implemented ball bouncing physics
 ## 9/13/25
 Decided that I am going to make volleyball game I think. However if it fails or I get done with it I might take the code and make a top down minigolf game with a ball that can go up vertically. I think volleyball could be cool because it takes the direction of Mario Tennis and changes it up a bit. 
+## 9/14/25 
+You can move around now.
+## 9/27/25
+Implemented Jump.
--- js/script.js
+++ js/script.js
@@ -36,8 +36,8 @@ const canvas = document.getElementById('canvas');
 
 const ctx = canvas.getContext('2d');
 let scalingFactor = 4;
-canvas.width =175 * scalingFactor;
-canvas.height = 100 * scalingFactor;
+canvas.width =128 * scalingFactor;
+canvas.height = 128 * scalingFactor;
 ctx.scale(scalingFactor, scalingFactor);
 
 let width = canvas.width / scalingFactor
@@ -64,7 +64,7 @@ class ball {
         this.pos.y += this.velocity.y * dt;
         this.pos.z += this.velocity.z * dt;
     
-        this.velocity.z -= this.gravity * dt; // gravity pulls down
+        this.velocity.z -= this.gravity * dt;
     
         if (this.pos.z <= 0) {
             this.pos.z = 0;
@@ -74,21 +74,56 @@ class ball {
 
     draw() {
         ctx.fillRect(this.pos.x, this.pos.y, 2, 2);
-        ctx.fillRect(this.pos.x - 1, this.pos.y-this.pos.z, 4, 4);
+        // Change this later to subtract by size of the ball. 9/17/25
+        ctx.fillRect(this.pos.x - 1, this.pos.y-2-this.pos.z, 4, 4);
+    }
+}
+
+class character {
+    constructor() {
+        this.pos = vec2(width -20, height -20);
+        this.pos.z = 0;
+        this.velocity = vec2(0,0);
+        this.velocity.z = 3;
+        this.friction = 1;
+        this.speed = 25;
+        this.length = 8;
+        this.width = 4;
+        this.gravity = 20;
+    }
+    update() {
+        this.pos.z += this.velocity.z *dt;
+        this.velocity.z -= this.gravity * dt; // gravity pulls down
+        if (this.pos.z <= 0) {
+            this.pos.z = 0;
+            this.velocity.z = 0;
+        }
+        console.log(this.pos.z)
+    }
+    draw() {
+        ctx.fillRect(this.pos.x, this.pos.y-this.pos.z, this.width, this.length);
+        // Fix this width stuff.
+        ctx.fillRect(this.pos.x + (this.width -(this.width+1) ), this.pos.y-(this.length /2)+this.length, this.width +2, 4);
     }
 }
 let Ball = new ball(vec2(50, 90));
+let Character = new character();
+
 let lastTime = performance.now();
 let dt = 0;
 function gameUpdate() {
     let now = performance.now();
     dt = (now - lastTime) / 1000; // convert ms to seconds
     lastTime = now;
+    Ball.update();
+    updateInput(dt);
+    Character.update();
+
 }
 
 function gameDraw() {
     Ball.draw();
-    Ball.update();
+    Character.draw();
 }
 
 function gameLoop() {
@@ -99,17 +134,63 @@ function gameLoop() {
     window.requestAnimationFrame(gameLoop);
     
 }
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false,
+    w: false,
+    s: false,
+    a: false,
+    d: false,
+    " ":false,
+};
 
-document.addEventListener("keydown", e => {
-    if (e.key === "ArrowUp") { // spacebar hit
-        Ball.velocity.z = 15;             // go upward
-        Ball.velocity.y = -15;     // move forward
-    }
-
-    if (e.key === "ArrowDown") { // spacebar hit
-        Ball.velocity.z = 15;             // go upward
-        Ball.velocity.y = 15;     // move forward
+window.addEventListener("keydown", (e) => {
+    if (keys.hasOwnProperty(e.key)) {
+        keys[e.key] = true;
     }
 });
+window.addEventListener("keyup", (e) => {
+    if (keys.hasOwnProperty(e.key)) {
+        keys[e.key] = false;
+    }
+});
+
+function updateInput(dt) {
+    let speed = Character.speed * dt;
+
+    if (keys.ArrowUp || keys.w) {
+        Character.pos.y -= speed;
+    }
+    if (keys.ArrowDown || keys.s) {
+        Character.pos.y += speed;
+    }
+
+    if (keys.ArrowLeft || keys.a) {
+        Character.pos.x -= speed;
+    }
+    if (keys.ArrowRight || keys.d) {
+        Character.pos.x += speed;
+    }
+
+    if (keys[" "]) {
+        console.log("HI");
+        Character.pos.z += speed;
+    }
+}
+//document.addEventListener("keydown", e => {
+//    if (e.key === "ArrowUp") { // spacebar hit
+//        Ball.velocity.z = 15;             // go upward
+//        Ball.velocity.y = -15;     // move forward
+//
+//        Character.velocity.y += 2;
+//    }
+//
+//    if (e.key === "ArrowDown") {
+//        Ball.velocity.z = 15; 
+//        Ball.velocity.y = 15;   
+//    }
+//});
 
 gameLoop();
